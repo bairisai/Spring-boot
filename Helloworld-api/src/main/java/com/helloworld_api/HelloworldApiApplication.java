35688736f3ce8c9e90deea379646bb02ae765ec6
krish <krishnasaibairi56140@gmail.com>
2025-09-09 09:30:10 +0530

    Helloworld-api done

---
+++ Helloworld-api/src/main/java/com/helloworld_api/HelloworldApiApplication.java
@@ -0,0 +1,13 @@
+package com.helloworld_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApiApplication.class, args);
+	}
+
+}
