0b4b22f58a64822bcc8747028a20a4c6af9193fd
krish <krishnasaibairi56140@gmail.com>
2025-09-29 13:56:55 +0530

    Product endpoints done

---
+++ ecom-application/src/main/java/com/app/ecom/service/ProductService.java
@@ -0,0 +1,77 @@
+package com.app.ecom.service;
+
+import com.app.ecom.dto.ProductRequest;
+import com.app.ecom.dto.ProductResponse;
+import com.app.ecom.model.Product;
+import com.app.ecom.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        Product product = new Product();
+        updateProductFromRequest(product, productRequest);
+        Product savedProduct = productRepository.save(product);
+        return mapToProductResponse(savedProduct);
+    }
+
+    public Optional<ProductResponse> updateProduct(Long id, ProductRequest productRequest) {
+        return productRepository.findById(id)
+                .map(existingProduct -> {
+                    updateProductFromRequest(existingProduct, productRequest);
+                    Product updatedProduct = productRepository.save(existingProduct);
+                    return mapToProductResponse(updatedProduct);
+                });
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findByActiveTrue().stream()
+                .map(this::mapToProductResponse)
+                .collect(Collectors.toList());
+    }
+
+    private ProductResponse mapToProductResponse(Product savedProduct) {
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.setId(savedProduct.getId());
+        productResponse.setName(savedProduct.getName());
+        productResponse.setDescription(savedProduct.getDescription());
+        productResponse.setPrice(savedProduct.getPrice());
+        productResponse.setCategory(savedProduct.getCategory());
+        productResponse.setImageUrl(savedProduct.getImageUrl());
+        productResponse.setStockQuantity(savedProduct.getStockQuantity());
+        productResponse.setActive(savedProduct.getActive());
+        return productResponse;
+    }
+
+    private void updateProductFromRequest(Product product, ProductRequest productRequest) {
+        product.setName(productRequest.getName());
+        product.setPrice(productRequest.getPrice());
+        product.setCategory(productRequest.getCategory());
+        product.setDescription(productRequest.getDescription());
+        product.setImageUrl(productRequest.getImageUrl());
+        product.setStockQuantity(productRequest.getStockQuantity());
+    }
+
+
+    public boolean deleteProduct(Long id) {
+        return productRepository.findById(id)
+                .map(product -> {
+                    product.setActive(false);
+                    productRepository.save(product);
+                    return true;
+                }).orElse(false);
+    }
+
+    public List<ProductResponse> searchProducts(String keyword) {
+        return productRepository.searchProducts(keyword).stream().map(this::mapToProductResponse).collect(Collectors.toList());
+    }
+}
